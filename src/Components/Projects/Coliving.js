41820771ba820069adb82b772d41ececa7840872
jdolata <juliadolata007@gmail.com>
2023-07-04 15:33:40 +0200

    add rakos patak section

--- src/Components/Projects/Coliving.js
+++ src/Components/Projects/Coliving.js
@@ -1,2 +1,41 @@
-const Coliving = () => {};
+import ProjectLayout from "./ProjectLayout";
+
+//assets
+import cover from "../../Assets/Projects/CoLiving/cover.jpg";
+
+const Coliving = () => {
+  return (
+    <ProjectLayout
+      title="CO-LIVING
+      FOR THE
+      ELDERLY"
+      subtitle="Bachelor Thesis at Poznan
+      University of Technology
+      Faculty of Architecture"
+      cover={cover}
+      team="Julia Dolata, Katarzyna Śliwka"
+      year="2022"
+      nextproject="/rakos_patak"
+      description="The BSc Thesis includes a project of a residential-service
+      building located in Poznań, Jeżyce district. It incorporates
+      conceptual, construction, and structural design.
+      The project aims to address the contemporary social
+      issues prevalent in our society, such as loneliness and
+      isolation, which have been amplified by the recent pandemic
+      circumstances. The design prioritizes the needs
+      of solitary individuals, particularly the elderly, who constitute
+      a social group deserving of special consideration
+      and support.
+      Due to its unique character, the building stands out as
+      an innovative element of the urban fabric. The created
+      structure fits into the frontage along the street since it is
+      inspired by the local context. Moreover, with its modern
+      form, it invites people to enter the diverse green space of
+      the courtyard through the dynamic opening in elevation.
+      It aims to introduce a fresh perspective and innovative
+      solutions that are infrequently employed in the realm of
+      Polish housing."
+    />
+  );
+};
 export default Coliving;
--- src/Components/Projects/RakosPatak.js
+++ src/Components/Projects/RakosPatak.js
@@ -1,2 +1,33 @@
-const RakosPatak = () => {};
+import ProjectLayout from "./ProjectLayout";
+
+//assets
+import cover from "../../Assets/Projects/RakosPatak/coverscreen.png";
+
+const RakosPatak = () => {
+  return (
+    <ProjectLayout
+      title="Rakos Patak"
+      subtitle="Bike Square"
+      cover={cover}
+      team="Julia Dolata, Eva Le Bascond"
+      year="2021"
+      nextproject="/childrens_house"
+      description="Bike Square is an innovative project situated in close
+      proximity to Budapest, adjacent to the picturesque bike
+      line along Rakos Patak. Designed as a temporary wooden
+      modular structure, this unique development offers
+      versatility through its potential for reuse in different locations.
+      The project boasts a thoughtful design, featuring a blend
+      of open modules for communal spaces, activities, and
+      communication. Alongside dedicated modules transformed
+      into cozy bedrooms and safe indoor bike lockers,
+      catering to the needs of bikers seeking an overnight stay.
+      One of Bike Square’s remarkable advantages lies in its
+      close proximity to Budapest, making it an ideal base for
+      sightseeing ventures. Its unique location amidst nature
+      provides a tranquil environment, perfect for relaxation
+      and recreational activities."
+    />
+  );
+};
 export default RakosPatak;
